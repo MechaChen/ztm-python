adb119267db2b510facfb654bb431acead8d85fd
Benson Chen <tumlivein@gmail.com>
2025-11-09 13:11:14 +0800

    feat: Python Basics - Exercise : Type Conversion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# booleans
-name = 'Benson'
-is_cool = False
+birth_year = input('What year were you born?')
 
-is_cool = True
+age = 2025 - int(birth_year)
+print(f'You age is: {age}')
 
-print(bool(0))
-print(bool(1))
+age = 2025 - float(birth_year)
+print(f'You age is: {age}')
+
+age = 2025 - bool(birth_year)
+print(f'You age is: {age}')
