1a6d1dee0ef0c633bef4ccaab1b36cabb456bcfa
Benson Chen <tumlivein@gmail.com>
2025-11-13 01:11:27 +0800

    feat: Python Basics - List Methods 3

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-basket = ['a', 'b', 'c', 'd', 'e']
+basket = ['a', 'x', 'b', 'c', 'd', 'e']
+basket.sort() # mutate the original list
 
-# index()
-## 0th parameter is the value to find
-## 1st parameter is the start index of the finding zone
-## 2nd parameter is the end index of the finding zone
+print(basket)
+print(sorted(basket)) # create a sorted list
 
-print(basket.index('d', 0, 4))
-
-print('x' in basket)
-
-# work in string as well
-print('i' in 'hi my name is ion')
-
-print(basket.count('d'))
+new_basket = basket.copy()
+new_basket.reverse()
+print(new_basket)
