988b323c40504d4aeb0d627e25c766f4d84aacf5
Benson Chen <tumlivein@gmail.com>
2025-11-14 00:47:36 +0800

    feat: Python Basics - Sets 2

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# Sets
-# unordered collection of unique objects
+# Sets 2
 
-my_set = {1,2,3,4,5,5}
+my_set = {1,2,3,4,5}
+your_set = {4,5,6,7,8,9,10}
+
+print(my_set.difference(your_set))
 print(my_set)
 
-my_list = [1,2,3,4,5,5]
-print(set(my_list))
-
-print(len(my_set))
-
-new_set = my_set.copy()
-my_set.clear()
-
+print(my_set.discard(5))
 print(my_set)
-print(new_set)
+
+print(my_set.difference_update(your_set))
+print(my_set)
+
+my_set = {1,2,3,4,5}
+print(my_set.intersection(your_set))
+
+print(my_set.isdisjoint(your_set))
+
+print(my_set.union(your_set))
+print(my_set | your_set)
+
+my_set = {4,5}
+print(my_set.issubset(your_set))
+print(your_set.issuperset(my_set))
