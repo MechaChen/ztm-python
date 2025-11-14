f4ebaed14d1b7c6b3dee21275778b4d50fbd4a70
Benson Chen <tumlivein@gmail.com>
2025-11-14 23:59:46 +0800

    feat: Python Basics 2: Ternary Operator

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-# Conditional Logic
+# Ternary Operator
 
-is_old = False
-is_licenced = True
+is_friend = True
+can_message = "message allowed" if is_friend else "message not allowed"
 
-if is_old:
-  print('you are old enough to drive!')
-elif is_licenced:
-  print('you can drive now!')
-else:
-  print('you are not of age!')
-
-if is_old and is_licenced:
-  print('you are old enough to drive, and you have a licence!')
-else:
-  print('you are not of age!')
-
-print('okok')
+print(can_message)
