e38758988d29c21cb6a906a486098be7449d806c
Benson Chen <tumlivein@gmail.com>
2025-11-21 00:43:34 +0800

    feat: Python Basics 2: is vs ==

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-is_magician = False
-is_expert = False
+# Value Equality: Check if two objects have the same value after conversion
+print(True == 1)
+print('1' == 1)
+print([] == 1)
+print(10 == 10.00)
+print([] == []) # for Python, == for array and object will be deepEqual
 
-# chek if magician AND expert: "you are a master magician"
-if is_magician and is_expert:
-    print('you are a master magician')
+# Identity Equality: Check if two objects are the same object in memory
+print(True is 1)
+print('1' is 1)
+print([] is 1)
+print(10 is 10.00)
+print([] is [])
 
-# check if magician but not expert: "at least you're getting there"
-elif is_magician and not is_expert:
-    print("at least you're getting there")
-
-# if you're not a magician: "You need magic powers"
-elif not is_magician:
-    print('You need magic powers')
+a = [1,2,3]
+b = [1,2,3]
+print(a is b)
