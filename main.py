c37e18da91ef7f930fc7f942f6acc519a358f974
Benson Chen <tumlivein@gmail.com>
2025-12-08 01:43:03 +0800

    feat: Python Basics 2: Exercise: Find Duplicates

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-# Exercise!
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
+# Exercise: Check for duplicates in the list
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 
-# do not hint me
-fill = '$'
-empty = ' '
-for row in picture:
-  for pixel in row:
-    if pixel:
-      print(fill, end='')
-    else:
-      print(empty, end='')
-  print('\n', end='')
+set = set()
+duplicates = []
+
+for item in some_list:
+  if item in set:
+    duplicates.append(item)
+  else:
+    set.add(item)
+
+print(duplicates)
+
+
+duplicates_two = []
+
+for value in some_list:
+  if some_list.count(value) > 1:
+    if value not in duplicates_two:
+      duplicates_two.append(value)
+
+print(duplicates_two)
