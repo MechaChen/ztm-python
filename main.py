496fa9d03d8e19e2323bb330c0726ca9153955b0
Benson Chen <tumlivein@gmail.com>
2026-01-02 13:29:01 +0800

    feat: Advanced Python: Object Oriented Programming: Attributes and Methods

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # OOP
 class PlayerCharacter:
+  membership = True # class object attribute: belongs to the class, not the instance
+
   def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    if PlayerCharacter.membership: # can be accessed by the class or the instance
+      self.name = name
+      self.age = age
+
+  def shout(self):
+    print(f'my name is {self.name}')
 
   def run(self):
     print('run')
@@ -18,5 +24,5 @@ print(player2.age)
 print(player1)
 print(player2)
 
-print(player1.run())
+print(player1.shout())
 print(player1.run)
