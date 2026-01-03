55c8da66e0f11a617ffd2d8c966de0218400936c
Benson Chen <tumlivein@gmail.com>
2026-01-03 17:43:55 +0800

    feat: Advanced Python: Object Oriented Programming: Exercise: Cats Everywhere

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-# OOP
-class PlayerCharacter:
-  membership = True # class object attribute: belongs to the class, not the instance
-
-  def __init__(self, name='anonymous', age=0):
-    if age > 18: # can be accessed by the class or the instance
+# Given the below class:
+class Cat:
+  species = 'mammal'
+  def __init__(self, name, age):
       self.name = name
       self.age = age
 
-  def shout(self):
-    print(f'my name is {self.name}')
 
-  def run(self):
-    print('run')
-    return 'done'
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Benson', 32)
+cat2 = Cat('Claire', 38)
+cat3 = Cat('Bennet', 34)
 
 
-player1 = PlayerCharacter()
-player2 = PlayerCharacter('Claire', 18)
+# 2 Create a function that finds the oldest cat
+def get_oldest_cat(*cats):
+  oldest_cat = cats[0]
+  for cat in cats:
+    if cat.age > oldest_cat.age:
+      oldest_cat = cat
+  return oldest_cat
 
-print(player1.name)
-print(player2.age)
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 
-print(player1)
-print(player2)
-
-print(player1.shout())
-print(player1.run)
+oldest_cat = get_oldest_cat(cat1, cat2, cat3)
+print(f"The oldest cat is {oldest_cat.name} and is {oldest_cat.age} years old.")
