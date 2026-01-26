6200bff36a2b1b87c74725bf21c03e7690491d06
Benson Chen <tumlivein@gmail.com>
2026-01-26 13:34:55 +0800

    feat: Advanced Python: Object Oriented Programming:  Dunder Methods

--- main.py
+++ main.py
@@ -1,40 +1,26 @@
-class User:
-    def __init__(self, name, email, age):
-        self.name = name
-        self.age = age
-        self.email = email
+class Cat:
+    def __init__(self, name, age, color):
+        self.color = color
+        self.my_dict = {"name": "mi", "age": age}
 
-    def sign_in(self):
-        print("logged in")
+    def __str__(self):
+        return f"Fur color is {self.color}"
 
-    def attack(self):
-        print("do nothing")
+    def __len__(self):
+        return 13
 
+    def __call__(self):
+        return "meow~~"
 
-class Wizard(User):
-    def __init__(self, name, email, age, power):
-        super().__init__(name, email, age)
-        self.power = power
-
-    def attack(self):
-        User.attack(
-            self
-        )  # if we want to have both parent & child behavior, we can call parent method inside child method
-        print(f"attacking with power of {self.power}")
+    def __getitem__(self, index):
+        return self.my_dict[index]
 
 
-class Archer(User):
-    def __init__(self, name, age, email, num_arrows):
-        self.name = name
-        self.age = age
-        self.num_arrows = num_arrows
-
-    def attack(self):
-        print(f"attacking with arrows: arrows left - {self.num_arrows}")
-
-
-wizard1 = Wizard("Claire", "chillymood@gmail.com", 38, 100)
-archer1 = Archer("Benson", "tumlivein@gmail.com", 32, 10)
-
-print(dir(wizard1))
-print(dir(archer1))
+scottish_fold = Cat("Scottish Fold", 5, "grey")
+print(scottish_fold)
+print(str(scottish_fold))
+print(scottish_fold.__str__())
+print(str("Hello"))
+print(scottish_fold())
+print(len(scottish_fold))
+print(scottish_fold["name"])
